8d53b9dcfde5d701ff922965a98a4e43691d0407
brian <liar88828@gmail.com>
2024-08-22 18:00:24 +0700

    init

---
+++ app/src/main/java/com/tutor/fashion_e_commerce/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.tutor.fashion_e_commerce
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tutor.fashion_e_commerce.ui.theme.Fashion_ECommerceTheme
+
+class MainActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		enableEdgeToEdge()
+		setContent {
+			Fashion_ECommerceTheme {
+				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+					Greeting(
+						name = "Android",
+						modifier = Modifier.padding(innerPadding)
+					)
+				}
+			}
+		}
+	}
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+	Text(
+		text = "Hello $name!",
+		modifier = modifier
+	)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+	Fashion_ECommerceTheme {
+		Greeting("Android")
+	}
+}
